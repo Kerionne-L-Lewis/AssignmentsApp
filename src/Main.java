c2c43a0fe1a436386d81010dfabf7333be618165
Kerionne <31558880+Kerionne@users.noreply.github.com>
2018-02-17 13:13:16 -0600

    Output the current date-time.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.time.Clock;
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("\n\nHello, AssignmentsApp!\n");
+        System.out.println(LocalDateTime.now());
+
+
     }
 }
+
