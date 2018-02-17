45d540bc72b46b08956c7a9acbc151d8d5a7580a
Kerionne <31558880+Kerionne@users.noreply.github.com>
2018-02-17 13:35:59 -0600

    Output tomorrow's date using a formatter.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("\n\nHello, AssignmentsApp!\n");
-        System.out.println(LocalDateTime.now());
+        System.out.println("Today's Date: " + LocalDateTime.now());
+        tomorrowsDate();
 
+    }
 
+    private static void tomorrowsDate() {
+        LocalDateTime today = LocalDateTime.now();
+        LocalDateTime tomorrow=today.plusDays(1);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss");
+        String formatDateTime = tomorrow.format(formatter);
+        System.out.println("Tomorrow's Date:" +formatDateTime);
     }
 }
 
